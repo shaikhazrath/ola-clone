7e885304b73bf138b3f53a028443bbc7446acbf3
shaikhazrath <shaikhazrathali123@gmail.com>
2023-11-13 17:44:34 +0530

    showcarsstyling

--- main/models.py
+++ main/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from datetime import datetime
 
+
+
 class Car(models.Model):
     brand = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+
     rental_price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='car_images/', null=True, blank=True)
+    
+    
 
     def __str__(self):
         return f"{self.brand} {self.model}"
--- main/views.py
+++ main/views.py
@@ -10,11 +10,13 @@ def home(request):
 
 def getcars(request):
     if request.method == 'POST':
-        location = request.POST.get('location')
+        city = request.POST.get('city')
         p_date = request.POST.get('pickupDate')
         d_date = request.POST.get('deliveryDate')
-        pickup_date = datetime.strptime(request.POST.get('pickupDate'), '%d-%m-%Y')
-        delivery_date = datetime.strptime(request.POST.get('deliveryDate'), '%d-%m-%Y')
+        pickup_date = datetime.strptime(request.POST.get('pickupDate'), '%Y-%m-%d')
+        delivery_date = datetime.strptime(request.POST.get('deliveryDate'), '%Y-%m-%d')
+
+
 
         if delivery_date.time() == datetime.min.time():
             delivery_date = delivery_date.replace(hour=23, minute=59, second=59)
@@ -24,17 +26,20 @@ def getcars(request):
             delivery_date__gte=pickup_date
         ).values_list('car', flat=True)
 
-        available_cars = Car.objects.filter(location=location).exclude(id__in=rented_cars)
+        available_cars = Car.objects.filter(city=city).exclude(id__in=rented_cars)
+        pickup_places =  available_cars.values_list('place', flat=True).distinct()
 
+        rental_days = (delivery_date - pickup_date).days + 1
         for car in available_cars:
-            rental_days = (delivery_date - pickup_date).days + 1
             car.total_price = rental_days * car.rental_price
-            
+
         return render(request, 'cars.html', {
             'available_cars': available_cars,
-            'location': location,
+            'city': city,
             'pickup_date': p_date,
             'delivery_date': d_date,
+            'pickup_places':pickup_places,
+            'rental_days':rental_days
         })
 
     return render(request, 'cars.html')
